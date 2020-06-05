61ef50ca377d550068c4e06853b5033250a4ee2b
thedeathwar73 <52138748+thedeathwar73@users.noreply.github.com>
2020-06-05 22:44:55 +0200

    Add files via upload

--- Serveur/CapitalisteAdventure/src/main/java/com/isis/CapitalisteAdventure/Services.java
+++ Serveur/CapitalisteAdventure/src/main/java/com/isis/CapitalisteAdventure/Services.java
@@ -1,7 +1,11 @@
 package com.isis.CapitalisteAdventure;
 
+import com.isis.CapitalisteAdventure.generated.PallierType;
+import com.isis.CapitalisteAdventure.generated.ProductType;
+import com.isis.CapitalisteAdventure.generated.ProductsType;
 import com.isis.CapitalisteAdventure.generated.World;
 
+import javax.validation.constraints.NotNull;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -9,20 +13,145 @@ import javax.xml.bind.Unmarshaller;
 import java.io.*;
 
 public class Services {
-    public World readWorldFromXml() throws JAXBException{
-        InputStream input = getClass().getClassLoader().getResourceAsStream("world.xml");
+    public World readWorldFromXml(String username) throws JAXBException{
+        InputStream input = null;
+        String file = "";
+        JAXBContext jaxbContext;
+        Unmarshaller u;
+        World w;
+
+        if (username != null){
+            file = username + "-" + "world.xml";
+            try{
+                input = new FileInputStream(file);
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (input == null){
+            input = getClass().getClassLoader().getResourceAsStream("world.xml");
+        }
+
+        if (input == null) {
+            return null;
+        }
+
+        try{
+            jaxbContext = JAXBContext.newInstance(World.class);
+            u = jaxbContext.createUnmarshaller();
+            w=(World) u.unmarshal(input);
+            input.close();
+            return w;
+        }
+        catch(JAXBException | IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+
+        /*getClass().getClassLoader().getResourceAsStream("world.xml");
         JAXBContext j = JAXBContext.newInstance(World.class);
         Unmarshaller u = j.createUnmarshaller();
-        return (World) u.unmarshal(input);
+        return (World) u.unmarshal(input);*/
     }
 
-    public void saveWorldToXml(World world) throws JAXBException, FileNotFoundException{
-        Marshaller marsh = JAXBContext.newInstance(World.class).createMarshaller();
-        marsh.marshal(world, new FileOutputStream("world.xml"));
+    public void saveWorldToXml(String Username, World world) throws JAXBException, FileNotFoundException{
+        OutputStream output;
+        JAXBContext j;
+        Marshaller m;
+
+        if (Username==null)
+            return;
+
+        String filename= Username + "-" + "world.xml";
+
+        try{
+            output = new FileOutputStream((filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            j=JAXBContext.newInstance(World.class);
+            m=j.createMarshaller();
+            m.marshal(world, output);
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*Marshaller marsh = JAXBContext.newInstance(World.class).createMarshaller();
+        marsh.marshal(world, new FileOutputStream("world.xml"));*/
 
     }
 
-    public World getWorld() throws JAXBException{
-        return readWorldFromXml();
+    public World getWorld(String username) throws JAXBException {
+        if (username == null)
+            username="";
+        World world =readWorldFromXml(username);
+        return readWorldFromXml(username);
     }
+
+    public ProductType findProductById(World w, int Id){
+         return w.getProducts().getProduct().get(Id);
+    }
+
+    public PallierType findManagerByName(World w, String name) {
+        return w.getManagers().getPallier().get(w.getManagers().getPallier().indexOf(name));
+
+    }
+
+    public Boolean updateProduct(String username, ProductType newproduct) throws JAXBException, FileNotFoundException {
+        World world = getWorld(username);
+        ProductType product = findProductById(world, newproduct.getId()) ;
+        if (product == null) { return false;}
+
+        int qtchange = newproduct.getQuantite() - product.getQuantite();
+        if (qtchange > 0) {
+            world.setMoney(world.getMoney()-qtchange*newproduct.getCout());
+        } else {
+            product.setTimeleft(newproduct.getVitesse());
+        }
+        // sauvegarder les changements du monde
+        saveWorldToXml(username, world);
+        return true;
+    }
+
+
+
+    public Boolean updateManager(String username, PallierType newmanager) throws JAXBException, FileNotFoundException {
+        // aller chercher le monde qui correspond au joueur
+        World world = getWorld(username);
+        // trouver dans ce monde, le manager équivalent à celui passé
+        // en paramètre
+        PallierType manager = findManagerByName(world, newmanager.getName());
+        if (manager == null) {
+            return false;
+        }
+
+        // débloquer ce manager
+        manager.setUnlocked(true);
+
+        // trouver le produit correspondant au manager
+        ProductType product = findProductById(world, manager.getIdcible());
+        if (product == null) {
+            return false;
+        }
+        // débloquer le manager de ce produit
+        newmanager.setUnlocked(true);
+
+        // soustraire de l'argent du joueur le cout du manager
+        world.setMoney(world.getMoney()-newmanager.getSeuil());
+
+        // sauvegarder les changements au monde
+        saveWorldToXml(username, world);
+        return true;
+    }
+
+
+
+
 }
--- Serveur/CapitalisteAdventure/src/main/java/com/isis/CapitalisteAdventure/Webservices.java
+++ Serveur/CapitalisteAdventure/src/main/java/com/isis/CapitalisteAdventure/Webservices.java
@@ -1,11 +1,17 @@
 package com.isis.CapitalisteAdventure;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.isis.CapitalisteAdventure.generated.PallierType;
+import com.isis.CapitalisteAdventure.generated.ProductType;
+import com.isis.CapitalisteAdventure.generated.ProductsType;
+import com.isis.CapitalisteAdventure.generated.World;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
@@ -15,11 +21,38 @@ import java.io.IOException;
         public Webservices() {
             services = new Services();
         }
+
         @GET
         @Path("world")
         @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-        public Response getWorld() throws JAXBException {
-            return Response.ok(services.getWorld()).build();
+        public Response getWorld(@Context HttpServletRequest request) throws JAXBException, FileNotFoundException {
+            String username = request.getHeader("X-user");
+            World world = services.getWorld(username);
+            services.saveWorldToXml(username, world);
+            return Response.ok(world).build();
         }
 
-    }
+        @PUT
+        @Path("product")
+        @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+        public Response putProduct(@Context HttpServletRequest request, ProductType product) throws JAXBException, FileNotFoundException {
+            if(services.updateProduct(request.getHeader("X-user"), product)){
+                return Response.ok("").build();
+            }
+            else {
+                return Response.notModified().build();
+            }
+        }
+
+        @PUT
+        @Path("manager")
+        @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+        public Response putManager(@Context HttpServletRequest request, PallierType manager) throws JAXBException, FileNotFoundException {
+            if(services.updateManager(request.getHeader("X-user"), manager)){
+                return Response.ok("").build();
+            }
+            else {
+                return Response.notModified().build();
+            }
+        }
+}
